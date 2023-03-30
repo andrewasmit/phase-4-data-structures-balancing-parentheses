15202701103bc302942948f21e503f51122fe6a6
andrew <andrewasmit@hotmail.com>
2023-03-29 19:38:44 -0500

    commit tree 9a1ff933e5f93ef3af99f14817add414088b255b

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,22 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+    counter = 0
+    string.chars.each do |char|
+        if char == "(" then
+            stack.push(char)
+            counter+=1
+        else
+            stack.push(char)
+            counter -=1
+        end
+    end 
+    if counter == 0 && stack.peek == "("
+        2
+    elsif counter < 0
+        counter * -1
+    else
+        counter
+    end
+end
